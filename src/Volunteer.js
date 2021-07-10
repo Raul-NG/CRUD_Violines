5f8fedd9df67e02a41b97968748ca7f46bbfea08
Raul-NG <raulng081@gmail.com>
2021-07-09 23:21:24 -0500

    VideoYT2 Listo. Búsqueda Lista.

---
+++ src/Volunteer.js
@@ -0,0 +1,26 @@
+class Volunteer{
+    constructor(_id, _name, _region, _department, _period, _project, _campus){
+        this.id = _id;
+        this.name = _name;
+        this.region = _region;
+        this.department = _department;
+        this.period = _period;
+        this.project = _project;
+        this.campus = _campus;
+
+        this.registerTimeStamp = "";
+        this.ingressType = "";
+        this.institutionalEmail = "";
+        this.personalEmail = "";
+        this.whatsApp = "";
+        this.career = "";
+        this.talent = "";
+        this.hours = "";
+        this.CAG = "";
+        this.folder = "";
+    }
+
+    show(){
+        return [this.id, this.name, this.region, this.department, this.period, this.project, this.campus];
+    }
+}
--- src/serverSideFuncs.js
+++ src/serverSideFuncs.js
@@ -1,7 +1,18 @@
-function getDataForSearch(){
-    // Optimizar
-    const spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
-    const workSheet = spreadSheet.getSheetByName("Voluntarios");
-    Logger.log(workSheet);
-    return workSheet.getRange(2,1,workSheet.getLastRow() - 1, 15).getValues(); // workSheet.getDataRange().getValues();
+function getDataForSearch() {
+    const workSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Voluntarios");
+    return workSheet.getRange(2, 1, workSheet.getLastRow() - 1, 17).getValues().map((row) => {
+        return new Volunteer(
+            _id         =   row[5],
+            _name       =   row[4],
+            _region     =   row[2],
+            _department =   row[11],
+            _period     =   row[3],
+            _project    =   row[12],
+            _campus     =   row[10]
+        );
+    });
+}
+
+function prueba() {
+    Logger.log(getDataForSearch());
 }
